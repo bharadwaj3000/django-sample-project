781b7d62d6662d766fa782b17ea4119ef72e52a1
Bharadwaj Narayana <bharadwajnarayana@Bharadwajs-MacBook-Pro.local>
2022-07-02 12:55:41 +0530

    added some files

--- airlines/airlines/settings.py
+++ airlines/airlines/settings.py
@@ -34,12 +34,14 @@ INSTALLED_APPS = [
     'flights',
     'users',
     'todo',
+    'inc_count',
     'django.contrib.admin',
     'django.contrib.auth',
+    "crispy_forms",
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'django.contrib.staticfiles',
+    'django.contrib.staticfiles'
 ]
 
 MIDDLEWARE = [
@@ -84,6 +86,8 @@ DATABASES = {
 }
 
 
+
+
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
 
@@ -121,6 +125,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+CRISPY_TEMPLATE_PACK = "bootstrap4"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
--- airlines/airlines/urls.py
+++ airlines/airlines/urls.py
@@ -13,6 +13,7 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
 from django.urls import path,include
 
@@ -20,5 +21,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path("flights/",include("flights.urls", namespace='flights')),
     path("users/", include("users.urls", namespace='users')),
-    path('todo/', include('todo.urls', namespace='todo'))
+    path('todo/', include('todo.urls', namespace='todo')),
+    path('inc_count/', include('inc_count.urls', namespace='inc_count'))
 ]
